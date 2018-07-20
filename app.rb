462839349bcab864aac54efa8c2990234fecef91
reeseglasscock <reesegpdx@gmail.com>
2018-07-20 12:54:30 -0700

    new stores being added

--- app.rb
+++ app.rb
@@ -13,5 +13,13 @@ get('/stores') do
 end
 
 get('/admin') do
+  @stores = Store.all
   erb(:admin)
+end
+
+post('/admin/stores/:id') do
+  store = params.fetch("new_store")
+  @store = Store.create({:store => store})
+  @stores = Store.all
+  redirect '/admin'
 end

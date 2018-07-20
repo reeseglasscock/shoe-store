0a348fc7a2b553ee1aec2cfdf1417f9945276b2e
reeseglasscock <reesegpdx@gmail.com>
2018-07-20 12:44:01 -0700

    update admin portal for adding a new store

--- app.rb
+++ app.rb
@@ -7,3 +7,11 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file}
 get('/') do
   erb(:index)
 end
+
+get('/stores') do
+  erb(:stores)
+end
+
+get('/admin') do
+  erb(:admin)
+end

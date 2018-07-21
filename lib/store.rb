7da8c0fb504b4113eba8d846d15902c4c07c4d75
reeseglasscock <reesegpdx@gmail.com>
2018-07-20 17:11:57 -0700

    stores will now titleize when saving before validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,10 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many :brands
+  before_validation :normalize_name, on: :create
   validates(:store, :presence => true)
+
+  private
+    def normalize_name
+      self.store = store.downcase.titleize
+    end
 end

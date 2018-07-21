b592d348d2704cea01845a206244843c5837dd4a
reeseglasscock <reesegpdx@gmail.com>
2018-07-20 17:17:07 -0700

    update vaidation for store to have maximum length

--- lib/store.rb
+++ lib/store.rb
@@ -1,7 +1,7 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many :brands
   before_validation :normalize_name, on: :create
-  validates(:store, {:presence => true, uniqueness: { case_sensitive: false }})
+  validates(:store, {:presence => true, uniqueness: { case_sensitive: false }, :length => {:maximum => 100}})
 
   private
     def normalize_name

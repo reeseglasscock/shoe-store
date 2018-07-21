ffb52ae2df476c38f7af6fafe9b878eccdcdfd9e
reeseglasscock <reesegpdx@gmail.com>
2018-07-20 22:20:56 -0700

    remove accessors for attributes in classes

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,6 +1,5 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many :stores
-  attr_accessor :shoe_brand, :price
   before_validation :normalize_name, on: :create
   validates(:shoe_brand, {:presence => true, :length => {:maximum => 100}, uniqueness: { case_sensitive: false }})
   validates(:price, {:presence => true})
--- lib/store.rb
+++ lib/store.rb
@@ -1,6 +1,5 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many :brands
-  attr_accessor :store
   before_validation :normalize_name, on: :create
   validates(:store, {:presence => true, uniqueness: { case_sensitive: false }, :length => {:maximum => 100}})
 
